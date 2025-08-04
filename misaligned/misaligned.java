22bed3706ef6c2a62ff658ab692c0e0e6e41d196
Yasaswini9 <134579216+Yas9aswini@users.noreply.github.com>
2025-08-04 10:17:33 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -5,7 +5,7 @@ public class Misaligned {
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
             for(j = 0; j < 5; j++) {
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
+                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[j]);
             }
         }
         return i * j;
@@ -16,3 +16,4 @@ public class Misaligned {
         System.out.println("All is well (maybe!)");
     }
 }
+
